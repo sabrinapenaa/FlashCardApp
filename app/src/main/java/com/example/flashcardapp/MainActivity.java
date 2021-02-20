ac688f1d791135edf4ac2571621f31ac8af0c00b
sabrinapenaa <sabrinapena.7721@gmail.com>
2021-02-20 13:22:14 -0600

    Setting OnCliCkListener to change visibility of question and answer

--- app/src/main/java/com/example/flashcardapp/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.flashcardapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        TextView flashcardQuestion= findViewById(R.id.flashcard_question);
+        //now want to set on click listener for when click question
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //need to set to visible
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
